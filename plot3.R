9c59a585acbcb0dc948353412097a7b9cdad412e
Joy <h52101019@gmail.com>
2016-08-23 09:16:03 +0800

    Modify plot3

--- plot3.R
+++ plot3.R
@@ -1,5 +1,8 @@
 source("loading_data.R")
 
+# Save the plot
+png("plot3.png", width = 480, height = 480)
+
 # Plotting
 with(data,{
   plot(x = Time, y = Sub_metering_1, type = "l",
@@ -7,10 +10,8 @@ with(data,{
   lines(x = Time, y = Sub_metering_2, col = "red")
   lines(x = Time, y = Sub_metering_3, col = "blue")
   legend("topright", legend = c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"),
-         col = c("black", "red", "blue"), lty = c(1, 1, 1), cex = 0.4)
+         col = c("black", "red", "blue"), lty = c(1, 1, 1), cex = 0.8)
 })
 
-# Save the plot
-dev.copy(png, "plot3.png", width = 480, height = 480)
 dev.off()
 
